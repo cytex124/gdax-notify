4ff1616311c9e1c47822e8131742932a93f2126f
Wyrobek Andreas <andreas.wyrobek@acp.de>
2017-11-03 15:16:45 +0100

    fix path of creds.iniadd better exceptionhandling, add browser arg, to open an b

--- src/core.py
+++ src/core.py
@@ -3,17 +3,28 @@ import gdax as gdax_lib
 from win10toast import ToastNotifier
 import logging
 import asyncio
+import os
+import sys
+import webbrowser
+import requests
+import json
+
+GDAX_URL = 'https://www.gdax.com/'
+
+file_path = os.path.dirname(os.path.realpath(__file__))
 
 logger = logging.getLogger('__GDAX__')
-toaster = ToastNotifier()
+WIN10_TOASTER = ToastNotifier()
 
 
 class GDax(object):
 
-    def __init__(self, interval=.5):
+    def __init__(self, interval=.5, browser=False):
         config = configparser.ConfigParser()
-        config.read('..\creds.ini')
+        config.read(os.path.join(file_path, '..\creds.ini'))
         credentials = dict(config['ACCESS'])
+
+        self.browser = browser
 
         self._open_orders = {}
         self._public_client = gdax_lib.PublicClient()
@@ -26,9 +37,6 @@ class GDax(object):
         self._loop = asyncio.get_event_loop()
         self._interval = interval
         self._handler_notify = None
-
-    def __del__(self):
-        self._loop.close()
 
     def _check_lost_orders(self, oos):
         check_order = []
@@ -58,24 +66,30 @@ class GDax(object):
                 fills = self._auth_client.get_fills(
                     order_id=check_order['id']
                 )[0]
+            except json.decoder.JSONDecodeError:
+                message = "Canceled {0}: {1} for {2} €".format(
+                    check_order['side'].upper(),
+                    check_order['size'],
+                    check_order['price']
+                )
+            else:
                 size = 0
                 for fill in fills:
                     size += float(fill['size'])
                 message = "Filled {0}: {1} for {2} €".format(
                     fills[0]['side'].upper(), size, fills[0]['price']
                 )
-                toaster.show_toast("GDax Fill", message, duration=5)
-            except Exception:
-                message = "Canceled {0}: {1} for {2} €".format(
-                    check_order['side'].upper(),
-                    check_order['size'],
-                    check_order['price']
-                )
+                self.note(message)
+                self.open_gdax_website()
             del self._open_orders[check_order['id']]
             logger.info(message)
 
     def _check_fills(self, loop=True):
-        oos = self._auth_client.get_orders()[0]
+        try:
+            oos = self._auth_client.get_orders()[0]
+        except requests.exceptions.ConnectionError:
+            logger.error("No Connection to GDax..")
+            sys.exit(1)
         lost_order = self._check_lost_orders(oos)
         self._add_new_orders(oos)
         self._notify_if_new_fills(lost_order)
@@ -88,5 +102,15 @@ class GDax(object):
         )
 
     def start_notify(self):
-        toaster.show_toast("GDax Notify", "Started...")
+        logger.info("GDax Notify started...")
+        self.note("Started...")
+        self.open_gdax_website()
         self._start_notify()
+
+    @staticmethod
+    def note(msg):
+        WIN10_TOASTER.show_toast("GDax Notify", msg)
+
+    def open_gdax_website(self):
+        if self.browser:
+            webbrowser.open_new_tab(GDAX_URL)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,27 @@
 from core import GDax
 import asyncio
 import logging
-
+import argparse
 
 logger = logging.getLogger('__GDAX__')
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--browser", action="store_true",
+                    help="Opens Webbrowser on filled Orders")
+args = parser.parse_args()
+
+
 if __name__ == '__main__':
-    gdax = GDax()
+    gdax = GDax(browser=args.browser)
     gdax.start_notify()
     try:
         asyncio.get_event_loop().run_forever()
     except KeyboardInterrupt:
-        print('Loop stopped')
-
+        logger.info('GDax Notify stopped...')
